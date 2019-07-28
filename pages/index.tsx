272b94de772d0ef18611ce9962986efdbdc31a90
Ali <mfateminia@gmail.com>
2019-07-28 16:01:42 +0200

    feat: styled-components added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import getConfig from 'next/config';
+import styled from 'styled-components';
+
+const StyledDiv =  styled.div`
+  font-size: 20px;
+`;
 
 const Index = () => {
   const { publicRuntimeConfig } = getConfig();
-    return <div>The environment is: <strong>{publicRuntimeConfig.env}</strong></div>;
+    return <StyledDiv>The environment is: <strong>{publicRuntimeConfig.env}</strong></StyledDiv>;
   }
   
 export default Index;
