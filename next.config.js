f366ad42ca4bcf9c302628c967460583e4032c6e
Ali <mfateminia@gmail.com>
2019-08-18 23:33:39 +0200

    feat: init

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const withSass = require('@zeit/next-sass');
-const withTypescript = require("@zeit/next-typescript")
 const extraConfig = require('./next.extra.config');
 
-module.exports = withSass(withTypescript({
+module.exports = withSass({
     ...extraConfig
-}));
+});
--- src/api/test.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const testApi = async () => {
-    const result = await axios.get('/api/v1/test');
-    console.log(result);
-}
