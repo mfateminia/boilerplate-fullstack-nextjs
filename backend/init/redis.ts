eb075395f88dae0926dd9493cc1e4aeb7bec6001
Ali <mfateminia@gmail.com>
2019-08-18 14:11:32 +0200

    wip

--- backend/init/redis.ts
+++ backend/init/redis.ts
@@ -3,22 +3,34 @@ import redis, {RedisClient} from 'redis';
 class Redis {
     public db: RedisClient;
 
-    constructor() {
-
-        this.init();
-    }
-
-    private init = async() => {    
+    public constructor() {
         this.db = redis.createClient();
  
-        this.db .on('connect', (err) => {
+        this.db.on('connect', () => {
             console.log('Redis connected!');
         })
          
-        this.db .on("error", (err) => {
+        this.db.on("error", () => {
+            console.log('Redis connection failed!');
+        });
+        this.init();
+    }
+
+    private init = async() => {
+        if (this.db) {
+            return;
+          }
+ 
+        this.db = redis.createClient();
+ 
+        this.db.on('connect', () => {
+            console.log('Redis connected!');
+        })
+         
+        this.db.on("error", () => {
             console.log('Redis connection failed!');
         });
     }
 }
 
-export default new Redis();
+export const redisInstance = new Redis();
